30e183747d63d433947cf32f77bb5701a83bfcb4
John Xander <johnblox5@gmail.com>
2022-10-17 10:17:57 +0100

    basic header

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import {
 import Login from './components/login/Login'
 import Register from './components/register/Register'
 import Games from './components/games/Games'
+import Header from './components/header/Header'
+import Landing from './components/landing/Landing'
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Router>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<Landing />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/games' element={<Games />} />
         </Routes>
