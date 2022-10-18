29235e0244444c0ae521e39139cbc5d2c2ea35ce
John Xander <johnblox5@gmail.com>
2022-10-18 14:55:15 +0100

    questionable round view

--- src/components/rounds/Rounds.js
+++ src/components/rounds/Rounds.js
@@ -2,10 +2,19 @@ import { useState } from 'react'
 import './style.css'
 
 const Rounds = ({ rounds }) => {
-    const [roundView, setRoundView] = useState(1)
+    const [roundView, setRoundView] = useState(undefined)
+    const [cards, setCards] = useState([])
+
+    const displayCards = (round) => {
+        const answers = rounds[round - 1].answers.join(',')
+        fetch(`http://localhost:4000/cards?words=${answers}`)
+            .then(res => res.json())
+            .then(data => setCards(data.cards))
+    }
 
     const handleClick = (round) => {
         setRoundView(round.id)
+        displayCards(round.id)
     }
 
     return (
@@ -17,14 +26,20 @@ const Rounds = ({ rounds }) => {
                     >
                         <p key={round.id}>{round.letter}</p>
                         {
-                            round.id === roundView ?
+                            round.id === roundView && roundView !== undefined ?
                                 <div
                                     style={{ backgroundColor: 'grey' }}
                                     className='answers'
                                     onClick={() => handleClick(round)}
                                 >
-                                    {round.answers.map((answer, i) => {
-                                        return <p key={i}>{answer} card</p>
+                                    {cards.map(card => {
+                                        return (
+                                            <div key={card.id} className="card">
+                                                <img src={card.imgUrl} alt={card.word} />
+                                                <h3>{card.word}</h3>
+                                                <p>{card.type}</p>
+                                            </div>
+                                        )
                                     })}
                                 </div> :
                                 <div
