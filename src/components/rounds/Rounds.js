b77ad37fd1bf02c93be1b43ada48a9d563e3e933
John Xander <johnblox5@gmail.com>
2022-10-18 12:30:34 +0100

    select round

--- src/components/rounds/Rounds.js
+++ src/components/rounds/Rounds.js
@@ -1,17 +1,42 @@
+import { useState } from 'react'
 import './style.css'
 
 const Rounds = ({ rounds }) => {
+    const [roundView, setRoundView] = useState(1)
+
+    const handleClick = (round) => {
+        setRoundView(round.id)
+    }
+
     return (
         <div className='rounds'>
             {rounds.map(round => {
                 return (
-                    <div className="round">
+                    <div
+                        className="round"
+                    >
                         <p key={round.id}>{round.letter}</p>
-                        <div className='answers'>
-                            {round.answers.map((answer, i) => {
-                                return <p key={i}>{answer}</p>
-                            })}
-                        </div>
+                        {
+                            round.id === roundView ?
+                                <div
+                                    style={{ backgroundColor: 'grey' }}
+                                    className='answers'
+                                    onClick={() => handleClick(round)}
+                                >
+                                    {round.answers.map((answer, i) => {
+                                        return <p key={i}>{answer} card</p>
+                                    })}
+                                </div> :
+                                <div
+                                    style={{ backgroundColor: '#212529' }}
+                                    className='answers'
+                                    onClick={() => handleClick(round)}
+                                >
+                                    {round.answers.map((answer, i) => {
+                                        return <p key={i}>{answer}</p>
+                                    })}
+                                </div>
+                        }
                     </div>
                 )
             })}
