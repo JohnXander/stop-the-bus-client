99950ee771aeeefd5347e15382147e14b77cfb39
John Xander <johnblox5@gmail.com>
2022-10-19 17:50:43 +0100

    simple navigation in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,12 @@
 import './style.css'
 
 const Header = ({ user }) => {
+
     return (
         <div className='header'>
-            <h3>Stop the Bus</h3>
+            <a className='app-name' href="/"><h3>Stop the Bus</h3></a>
             <div className='user'>
+                <a href="/games"><button>My Games</button></a>
                 <h6>{user.username}</h6>
                 <img
                     className='profile-pic'
