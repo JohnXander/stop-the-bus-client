20b48bf74df5729d3e96800a13f9c7c375dcd74b
John Xander <johnblox5@gmail.com>
2022-10-20 10:29:45 +0100

    change team points

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -15,6 +15,7 @@ const Game = () => {
     const [rounds, setRounds] = useState([])
     const [addRound, setAddRound] = useState(true)
     const [updateRound, setUpdateRound] = useState(true)
+    const [updateTeam, setUpdateTeam] = useState(true)
 
     useEffect(() => {
         if (id !== undefined) {
@@ -30,7 +31,7 @@ const Game = () => {
                 .then(res => res.json())
                 .then(data => setRounds(data.rounds))
         }
-    }, [id, addRound, updateRound])
+    }, [id, addRound, updateRound, updateTeam])
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -58,7 +59,7 @@ const Game = () => {
         <div>
             <h1>{name}</h1>
             <div className="game">
-                <Teams teams={teams} />
+                <Teams updateTeam={updateTeam} setUpdateTeam={setUpdateTeam} teams={teams} />
                 <div>
                     <Categories categories={categories} />
                     <Rounds updateRound={updateRound} setUpdateRound={setUpdateRound} rounds={rounds} />
--- src/components/teams/Teams.js
+++ src/components/teams/Teams.js
@@ -1,13 +1,35 @@
 import './style.css'
 
-const Teams = ({ teams }) => {
+const Teams = ({ teams, updateTeam, setUpdateTeam }) => {
+
+    const handleClick = (team, dir) => {
+        const { id, name, points, gameId } = team
+        let newPoints
+
+        if (dir === 'inc') newPoints = points + 1
+        if (dir === 'dec') newPoints = points - 1
+
+        fetch(`http://localhost:4000/teams/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, points: newPoints, gameId }),
+        })
+            .then(_ => setUpdateTeam(!updateTeam))
+    }
+
     return (
         <div className='teams'>
             {teams.map(team => {
                 return (
                     <div className='team' key={team.id}>
                         <h3>{team.name}</h3>
-                        <p className='points'>{team.points} pts</p>
+                        <div className='points'>
+                            <p>{team.points} pts</p>
+                            <button onClick={() => handleClick(team, 'inc')}>+</button>
+                            <button onClick={() => handleClick(team, 'dec')}>-</button>
+                        </div>
                     </div>
                 )
             })}
