13db08e2ab9dba3969d7f2b93068596fa0861e92
John Xander <johnblox5@gmail.com>
2022-10-24 11:15:29 +0100

    add style to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,44 @@
+import { useState } from 'react'
 import './style.css'
 
 const Header = ({ user }) => {
+    const [isHovering, setIsHovering] = useState(false)
+
+    const handleMouseOver = () => setIsHovering(true)
+    const handleMouseOut = () => setIsHovering(false)
 
     return (
         <div className='header'>
-            <a className='app-name' href="/"><h3>Stop the Bus</h3></a>
+            <a className='app-name' href="/">
+                <h3>Stop the Bus</h3>
+            </a>
             <div className='user'>
-                <a href="/games"><button>My Games</button></a>
-                <a href="/cards"><button>My Cards</button></a>
-                <h6>{user.username}</h6>
+                <div className='nav-link'>
+                    <i class="fa-solid fa-chalkboard"></i>
+                    <a href="/games">GAMES</a>
+                </div>
+                <div className='nav-link'>
+                    <i class="fa-solid fa-note-sticky"></i>
+                    <a href="/cards">CARDS</a>
+                </div>
                 <img
                     className='profile-pic'
                     src={user.imgUrl}
                     alt="profile pic"
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
                 />
+                {
+                    isHovering &&
+                    <div
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                        className='profile-info'
+                    >
+                        <h5>{user.username}</h5>
+                        <p>Total Games:</p>
+                    </div>
+                }
             </div>
         </div>
     )
